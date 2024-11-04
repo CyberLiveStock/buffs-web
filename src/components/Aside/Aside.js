c2d615cd0a9472bffcd66727ba61c661ad27936f
paulocsa <paulocandiani04@gmail.com>
2024-11-04 20:39:13 -0300

    feat: implementado item de menu ativo com base na rota atual
    - Implementada funcionalidade para destacar o item de menu ativo na barra lateral com base na rota atual.
    - Utilizado window.location.pathname para determinar a URL atual e definir o item ativo correspondente.
    - Garantido que o item 'Home' esteja ativo por padrão ao carregar a aplicação.
    - Adicionados ouvintes de eventos para atualizar o item ativo ao clicar, melhorando a experiência do usuário.

--- src/components/Aside/Aside.js
+++ src/components/Aside/Aside.js
@@ -1,18 +1,134 @@
 // src/components/Aside/Aside.js
 
-import styles from './Aside.module.css';
+import { useEffect } from "react";
+import styles from "./Aside.module.css";
 
 const Aside = () => {
+  useEffect(() => {
+    const menuItems = document.querySelectorAll(`.${styles.menuList} li`);
+    const currentPath = window.location.pathname; // Obtém a rota atual
+
+    // Função para definir o item ativo
+    const setActiveItem = (item) => {
+      menuItems.forEach((i) => i.classList.remove(styles.active));
+      item.classList.add(styles.active);
+    };
+
+    // Define o item ativo com base na rota atual
+    switch (currentPath) {
+      case "/":
+        setActiveItem(menuItems[0]); // Home
+        break;
+      case "/demandas":
+        setActiveItem(menuItems[1]); // Demandas
+        break;
+      case "/funcionarios":
+        setActiveItem(menuItems[2]); // Funcionários
+        break;
+      case "/bubalinos":
+        setActiveItem(menuItems[3]); // Bubalinos
+        break;
+      case "/controle-zootecnico":
+        setActiveItem(menuItems[4]); // Controle Zootécnico
+        break;
+      case "/controle-sanitario":
+        setActiveItem(menuItems[5]); // Controle Sanitário
+        break;
+      case "/controle-reproducao":
+        setActiveItem(menuItems[6]); // Controle de Reprodução
+        break;
+      default:
+        setActiveItem(menuItems[0]); // Fallback para Home
+    }
+
+    // Adiciona o evento de clique a cada item
+    menuItems.forEach((item) => {
+      item.addEventListener("click", () => {
+        setActiveItem(item); // Define o item ativo ao clicar
+      });
+    });
+
+    // Cleanup: remove event listeners ao desmontar o componente
+    return () => {
+      menuItems.forEach((item) => {
+        item.removeEventListener("click", () => {});
+      });
+    };
+  }, []);
+
   return (
     <div className={styles.aside}>
       <ul className={styles.menuList}>
-        <li><a href="#">Pagina Inicial</a></li>
-        <li><a href="#">Demandas</a></li>
-        <li><a href="#">Funcionarios</a></li>
-        <li><a href="#">Bubalinos</a></li>
-        <li><a href="#">Controle Zootécnico</a></li>
-        <li><a href="#">Controle Sanitário</a></li>
-        <li><a href="#">Controle de reprodução</a></li>
+        <li>
+          <a href="/">
+            <img
+              src="/images/icons/home.svg"
+              alt="Página Inicial"
+              className={styles.icon}
+            />{" "}
+            Pagina Inicial
+          </a>
+        </li>
+        <li>
+          <a href="/demandas">
+            <img
+              src="/images/icons/demandas.svg"
+              alt="Demandas"
+              className={styles.icon}
+            />{" "}
+            Demandas
+          </a>
+        </li>
+        <li>
+          <a href="/funcionarios">
+            <img
+              src="/images/icons/funcionarios.svg"
+              alt="Funcionários"
+              className={styles.icon}
+            />{" "}
+            Funcionarios
+          </a>
+        </li>
+        <li>
+          <a href="/bubalinos">
+            <img
+              src="/images/icons/bubalinos.svg"
+              alt="Bubalinos"
+              className={styles.icon}
+            />{" "}
+            Bubalinos
+          </a>
+        </li>
+        <li>
+          <a href="/controle-zootecnico">
+            <img
+              src="/images/icons/statistics.svg"
+              alt="Controle Zootécnico"
+              className={styles.icon}
+            />{" "}
+            Controle Zootécnico
+          </a>
+        </li>
+        <li>
+          <a href="/controle-sanitario">
+            <img
+              src="/images/icons/statistics.svg"
+              alt="Controle Sanitário"
+              className={styles.icon}
+            />{" "}
+            Controle Sanitário
+          </a>
+        </li>
+        <li>
+          <a href="/controle-reproducao">
+            <img
+              src="/images/icons/statistics.svg"
+              alt="Controle de Reprodução"
+              className={styles.icon}
+            />{" "}
+            Controle de Reprodução
+          </a>
+        </li>
       </ul>
     </div>
   );
