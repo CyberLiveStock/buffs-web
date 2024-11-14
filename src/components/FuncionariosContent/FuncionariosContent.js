ddbc904639585183f0a3e6a9b068669d515fe543
V1niSouza <105008901+EzArmy@users.noreply.github.com>
2024-11-13 22:06:40 -0300

    Feat: Consumindo API, metodo GET, na Pagina Funcionarios

--- src/components/FuncionariosContent/FuncionariosContent.js
+++ src/components/FuncionariosContent/FuncionariosContent.js
@@ -1,10 +1,25 @@
-
+import axios from "axios";
+import { useEffect, useState } from "react"
 import styles from './FuncionariosContent.module.css';
 import HeaderFuncionarios from '../HeaderFuncionarios/HeaderFuncionarios';
 
 
-
 const FuncionariosContent = () => {
+  const [funcionarios, setFuncionarios] = useState([]);
+
+  useEffect(() => {
+    const fetchFuncionarios = async () => {
+      try {
+        const response = await axios.get("http://localhost:4000/funcionarios");
+        setFuncionarios(response.data.funcionarios); //'funcionarios' array de funcionarios
+        console.log(funcionarios)
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchFuncionarios(); //Chamando a função, para executar a função
+  }, []); // '[]' dependecia do useEffect
+
   return (
     <div className={styles.content}>
       <HeaderFuncionarios />
@@ -30,18 +45,18 @@ const FuncionariosContent = () => {
           <thead>
             <tr>
               <th scope="col" className={styles.headerCell}>Nome</th>
-              <th scope="col" className={styles.headerCell}>CPF</th>
               <th scope="col" className={styles.headerCell}>Cargo</th>
-              <th scope="col" className={styles.headerCell}></th>
-              <th scope="col" className={styles.headerCell}></th>
-              <th scope="col" className={styles.headerCell}></th>
               <th scope="col" className={styles.headerCell}>Editar / Deletar</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td colSpan="7" className="text-center">Patrão vai mexer futuramente</td>
+          {funcionarios.map((funcionario) => (
+            <tr key={funcionario._id}>
+              <td className="text-center">{funcionario.nome}</td>
+              <td className="text-center">{funcionario.descCargo}</td>
+              <td className="text-center">AAA</td>
             </tr>
+          ))}
           </tbody>
         </table>
       </div>
