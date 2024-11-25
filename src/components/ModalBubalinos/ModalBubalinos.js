3df6d8878872470fc9328e305349fca0be92a826
paulocsa <paulocandiani04@gmail.com>
2024-11-24 21:21:45 -0300

    feat: modals de bubalinos

--- src/components/ModalBubalinos/ModalBubalinos.js
+++ src/components/ModalBubalinos/ModalBubalinos.js
@@ -5,7 +5,7 @@ const ModalBubalinos = ({ isOpen, closeModal, children }) => {
   if (!isOpen) return null;
 
   return (
-    <div className={styles.overlay} onClick={closeModal}>
+    <div className={styles.overlay}>
       <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
         <button className={styles.closeButton} onClick={closeModal}>
           X
