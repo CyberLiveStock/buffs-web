5f1b3fbce186cc0fc78d9464724db7e862b4747a
paulocsa <paulocandiani04@gmail.com>
2024-11-25 22:21:03 -0300

    feat: criação dos graficos da tela demandas

--- src/components/DemandasContent/DemandasContent.js
+++ src/components/DemandasContent/DemandasContent.js
@@ -1,14 +1,85 @@
-import axios from "axios";
 import { useEffect, useRef, useState } from "react";
 import HeaderDemandas from "../HeaderDemandas/HeaderDemandas";
-import styles from './DemandasContent.module.css'
+import styles from "./DemandasContent.module.css";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
 
-const DemandasContent = () =>{
-    return(
-        <div className={styles.content}>
-            <HeaderDemandas/>
+// Registrar os componentes do Chart.js necessários
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const labels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"];
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: [65, 59, 80, 81, 56, 55, 40],
+      fill: false,
+      borderColor: "#CE7D0A", // Cor 1
+      tension: 0.1,
+    },
+    {
+      label: "Dataset 2",
+      data: [45, 39, 70, 61, 46, 45, 30],
+      fill: false,
+      borderColor: "#FFCF78", // Cor 2
+      tension: 0.1,
+    },
+    {
+      label: "Dataset 3",
+      data: [25, 49, 60, 71, 36, 35, 20],
+      fill: false,
+      borderColor: "#43310B", // Cor 3
+      tension: 0.1,
+    },
+  ],
+};
+
+const config = {
+  type: "line",
+  data: data,
+  options: {
+    plugins: {
+      legend: {
+        position: "bottom",
+      },
+    },
+  },
+};
+
+const DemandasContent = () => {
+  return (
+    <div className={styles.content}>
+      <HeaderDemandas />
+      <div className={styles.container}>
+        <div className={styles.chartBox}>
+          {/* Primeiro gráfico com 3 linhas */}
+          <Line data={data} options={config.options} width={300} height={300} />
         </div>
-    )
-}
+        <div className={styles.chartBox}>
+          {/* Segundo gráfico permanece inalterado */}
+          <Line data={data} options={config.options} width={300} height={300} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default DemandasContent;
