6c6e5804443f5c02781390f060870bec19bf92d3
Vinicius Souza <vsouza@academico.intra>
2024-11-27 13:47:14 -0300

    Fix: COnsumindo API em Demandas

--- src/components/DemandasContent/DemandasContent.js
+++ src/components/DemandasContent/DemandasContent.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import HeaderDemandas from "../HeaderDemandas/HeaderDemandas";
 import styles from "./DemandasContent.module.css";
 import { Line } from "react-chartjs-2";
@@ -12,6 +12,7 @@ import {
   Tooltip,
   Legend,
 } from "chart.js";
+import axios from "axios";
 
 // Registrar os componentes do Chart.js necessários
 ChartJS.register(
@@ -24,97 +25,183 @@ ChartJS.register(
   Legend
 );
 
-const labels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"];
-const data = {
-  labels: labels,
-  datasets: [
-    {
-      label: "Dataset 1",
-      data: [65, 59, 80, 81, 56, 55, 40],
-      fill: false,
-      borderColor: "#CE7D0A", // Cor 1
-      tension: 0.1,
-    },
-    {
-      label: "Dataset 2",
-      data: [45, 39, 70, 61, 46, 45, 30],
-      fill: false,
-      borderColor: "#FFCF78", // Cor 2
-      tension: 0.1,
-    },
-    {
-      label: "Dataset 3",
-      data: [25, 49, 60, 71, 36, 35, 20],
-      fill: false,
-      borderColor: "#43310B", // Cor 3
-      tension: 0.1,
-    },
-  ],
-};
-
-const config = {
-  type: "line",
-  data: data,
-  options: {
-    plugins: {
-      legend: {
-        position: "bottom",
-      },
-    },
-  },
-};
-
 const DemandasContent = () => {
-  const [demandas, setDemandas] = useState([]);
-  const [funcionarios, setFuncionarios] = useState([])
+  const [demandas1, setDemandas1] = useState([]);
+  const [chartData1, setChartData1] = useState({
+    labels: [],
+    datasets: [],
+  });
 
-  // Fetch para coleção Demandas
+  const [demandas2, setDemandas2] = useState([]);
+  const [chartData2, setChartData2] = useState({
+    labels: [],
+    datasets: [],
+  });
+   
+  
+  //Grafico Referente ao rendimento do funcionario
   useEffect(() => {
     const fetchDemandas = async () => {
       try {
-        const response = await axios.get("http://localhost:4000/demandas");
-        setDemandas(response.data.demandas); //'demandas' array de demandas
-        console.log(demandas)
+        const response = await axios.get("http://localhost:4000/demandasGrafFunc");
+        const demandas1 = response.data; // Array retornado da API
+        setDemandas1(demandas1);
+
+        // Grafico Rendimento Funcionario
+        // Processar os dados para o gráfico
+        const labels = [...new Set(demandas1.map((item) => `${item.mes}/${item.ano}`))]; // EixoX
+        // Organização dos dados - Ordenando Labels
+        labels.sort((a, b) => {
+          const [mesA, anoA] = a.split("/").map(Number); //Converte a String para Number
+          const [mesB, anoB] = b.split("/").map(Number); //Converte a String para Number
+          if (anoA === anoB) {
+            return mesA - mesB;  // Ordenar por mês se o ano for o mesmo
+          }
+          return anoA - anoB;  // Caso contrário, ordenar por ano
+        });
+
+        const funcionarios = [...new Set(demandas1.map((item) => item.funcionario))]; // EixoY
+        const datasets = funcionarios.map((funcionario, index) => {
+          const data = labels.map((label) => {
+            const [mes, ano] = label.split("/");
+            const demanda = demandas1.find(
+              (item) =>
+                item.funcionario === funcionario &&
+                item.mes === parseInt(mes) &&
+                item.ano === parseInt(ano)
+            );
+            return demanda ? demanda.totalDemandas : 0;
+          });
+
+          // Paletas de cores para o datasets(Linha de Funcionarios)
+          const colors = [
+            "#CE7D0A",
+            "#FFCF78",
+            "#43310B",
+          ];
+
+          return {
+            label: funcionario,
+            data,
+            fill: false,
+            borderColor: colors[index % colors.length], // Atribui cor ciclicamente
+            tension: 0.1,
+          };
+        });
+        
+        setChartData1({
+          labels,
+          datasets,
+          
+        });
       } catch (error) {
         console.log(error);
       }
     };
-    fetchDemandas(); // Chamando a função para executar a requisição
-  }, []); // '[]' dependência do useEffect
+    fetchDemandas();
+  }, []);
 
-  // Fetch para coleção Funcionarios
+  //Grafico Referente ao rendimento das Categorias
   useEffect(() => {
-    const fetchFuncionarios = async () => {
+    const fetchCategorias = async () => {
       try {
-        const response = await axios.get("http://localhost:4000/funcionarios");
-        setFuncionarios(response.data.funcionarios); //'funcionarios' array de funcionarios
-        console.log(funcionarios)
+        const response = await axios.get("http://localhost:4000/demandasGrafCat");
+        const demandas2 = response.data; // Array retornado da API
+        setDemandas2(demandas2);
+
+        // Grafico Rendimento Categorias
+        // Processar os dados para o gráfico
+        const labels = [...new Set(demandas2.map((item) => `${item.mes}/${item.ano}`))]; // EixoX
+        // Organização dos dados - Ordenando Labels
+        labels.sort((a, b) => {
+          const [mesA, anoA] = a.split("/").map(Number); //Converte a String para Number
+          const [mesB, anoB] = b.split("/").map(Number); //Converte a String para Number
+          if (anoA === anoB) {
+            return mesA - mesB;  // Ordenar por mês se o ano for o mesmo
+          }
+          return anoA - anoB;  // Caso contrário, ordenar por ano
+        });
+
+        const categorias = [...new Set(demandas2.map((item) => item.categoria))]; // EixoY
+        const datasets = categorias.map((categoria, index) => {
+          const data = labels.map((label) => {
+            const [mes, ano] = label.split("/");
+            const demanda = demandas2.find(
+              (item) =>
+                item.categoria === categoria &&
+                item.mes === parseInt(mes) &&
+                item.ano === parseInt(ano)
+            );
+            return demanda ? demanda.totalDemandas : 0;
+          });
+
+          // Paletas de cores para o datasets(Linha de Funcionarios)
+          const colors = [
+            "#CE7D0A",
+            "#FFCF78",
+            "#43310B",
+          ];
+
+          return {
+            label: categoria,
+            data,
+            fill: false,
+            borderColor: colors[index % colors.length], // Atribui cor ciclicamente
+            tension: 0.1,
+          };
+        });
+        
+        setChartData2({
+          labels,
+          datasets,
+          
+        });
       } catch (error) {
         console.log(error);
       }
     };
-    fetchFuncionarios(); // Chamando a função para executar a requisição
-  }, []); // '[]' dependência do useEffect
+    fetchCategorias();
+  }, []);
 
-  // useEffect(() => {
-  //   if (demandas.length > 0) {
-  //     const quantidadeFuncionarios = funcionario.filter((dado) => dado.sexo === "Macho").length;
-
-
-  //   },} [demandas]);
-
-
+  
   return (
     <div className={styles.content}>
       <HeaderDemandas />
       <div className={styles.container}>
         <div className={styles.chartBox}>
-          {/* Primeiro gráfico com 3 linhas */}
-          <Line data={data} options={config.options} width={300} height={300} />
+          {/* Gráfico dinâmico dos Funcionarios */}
+          <Line
+            data={chartData1}
+            options={{
+              plugins: {
+                legend: {
+                  position: "bottom",
+                },
+              },
+              responsive: true,
+              maintainAspectRatio: true, // Manter proporção
+            }}
+            width={500}
+            height={500}
+          />
         </div>
+
         <div className={styles.chartBox}>
-          {/* Segundo gráfico permanece inalterado */}
-          <Line data={data} options={config.options} width={300} height={300} />
+          {/* Gráfico dinâmico das Categorias */}
+          <Line
+            data={chartData2}
+            options={{
+              plugins: {
+                legend: {
+                  position: "bottom",
+                },
+              },
+              responsive: true,
+              maintainAspectRatio: true, // Manter proporção
+            }}
+            width={500}
+            height={500}
+          />
         </div>
       </div>
     </div>
